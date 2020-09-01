0d8a78cf45315e8163296c7cfe1e8a60e4c439d6
Aleksander Iksanov <a.iksanov@andersenlab.com>
2020-09-01 19:36:16 +0300

    Updates

---
+++ precache-manifest.3a9bf79fed1aeb44ef5622eadabf334f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893d01a09ad3263b39908961882f167b",
+    "url": "/songs_table/index.html"
+  },
+  {
+    "revision": "91c793a3d33dff1e4a44",
+    "url": "/songs_table/static/css/main.0e1eeb45.chunk.css"
+  },
+  {
+    "revision": "c3bd438419c65c9c4ff7",
+    "url": "/songs_table/static/js/2.abb57798.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/songs_table/static/js/2.abb57798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c793a3d33dff1e4a44",
+    "url": "/songs_table/static/js/main.da066527.chunk.js"
+  },
+  {
+    "revision": "6b590c309c908a37cfaf",
+    "url": "/songs_table/static/js/runtime-main.82900742.js"
+  },
+  {
+    "revision": "d58934fe7377dcd53e3e07dbfc05e71d",
+    "url": "/songs_table/static/media/view.d58934fe.png"
+  }
+]);
